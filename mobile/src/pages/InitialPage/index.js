7f33d8140312650237f1ee114dc7e4dd7d4959a2
rafaela <rafabernardo.r@gmail.com>
2020-12-13 02:21:25 -0300

    initial page components

--- mobile/src/pages/InitialPage/index.js
+++ mobile/src/pages/InitialPage/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
-export default function InitialPage(){
-   return (
-      <View>
-         <Text>Just a text</Text>
-      </View>
-   )
+export default function InitialPage() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem vindo!</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text>Cadastrar Planta</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text>Visualizar Plantas</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
---
+++ mobile/src/pages/InitialPage/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#13131a',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+   padding: 20,
+   margin: 10,
+   backgroundColor: 'red'
+  }
+});
