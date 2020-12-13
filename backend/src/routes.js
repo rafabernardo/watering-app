6a17c31df4d6397c2596917c5df369b452bb89e4
rafaela <rafabernardo.r@gmail.com>
2020-12-13 00:37:31 -0300

    insert plant and list plant from bd

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,31 @@
+const { request } = require('express');
 const express = require('express');
 
 const routes = express.Router();
+const connection = require('./database/connection');
 
 
-routes.post('/new_plant', (request, response) =>{
-    const data = request.body;
+routes.post('/new_plant', async (request, response) =>{
+    const { species, watering_date, humidity_level, acquisition_date, locate } = request.body;
 
-    console.log(data);
+    console.log(species);
 
-    return response.json();
+    await connection('plant').insert({
+        species,
+        locate,
+        humidity_level,
+        watering_date,
+        acquisition_date
+    })
+
+    return response.json({ species });
+});
+
+routes.get('/list_plants', async (request, response) => {
+
+    const plants = await connection('plant').select('*');
+
+    return response.json(plants);
 });
 
 module.exports = routes;
