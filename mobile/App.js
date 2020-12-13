e08c2302568f31e661514b6716545f4226219d4c
velosobr <linoc.veloso@gmail.com>
2020-12-13 00:40:14 -0300

    splash

--- mobile/App.js
+++ mobile/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5CDB95',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#EDF5E1',
+    fontSize: 40,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
