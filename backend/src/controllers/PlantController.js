e71e68df5fa0955f1043cb9de8fedbbd4356a08b
rafaela <rafabernardo.r@gmail.com>
2020-12-13 01:05:00 -0300

    improve of list api

---
+++ backend/src/controllers/PlantController.js
@@ -0,0 +1,36 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async create(req, res) {
+    const {
+      species,
+      watering_date,
+      humidity_level,
+      acquisition_date,
+      locate,
+    } = req.body;
+
+    await connection('plant').insert({
+      species,
+      locate,
+      humidity_level,
+      watering_date,
+      acquisition_date,
+    });
+
+    return res.json({ species });
+  },
+
+  async list(req, res) {
+    const { page = 1 } = req.query;
+
+    const [count] = await connection('plant').count();
+    const plants = await connection('plant')
+      .select('*')
+      .limit(5)
+      .offset((page - 1) * 5);
+
+      res.header("X-Total-Count", count["count(*)"]);
+    return res.json(plants);
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,31 +1,11 @@
-const { request } = require('express');
 const express = require('express');
+const PlantController = require('./controllers/PlantController');
 
 const routes = express.Router();
-const connection = require('./database/connection');
 
 
-routes.post('/new_plant', async (request, response) =>{
-    const { species, watering_date, humidity_level, acquisition_date, locate } = request.body;
+routes.post('/new_plant', PlantController.create);
 
-    console.log(species);
-
-    await connection('plant').insert({
-        species,
-        locate,
-        humidity_level,
-        watering_date,
-        acquisition_date
-    })
-
-    return response.json({ species });
-});
-
-routes.get('/list_plants', async (request, response) => {
-
-    const plants = await connection('plant').select('*');
-
-    return response.json(plants);
-});
+routes.get('/list_plants', PlantController.list);
 
 module.exports = routes;
