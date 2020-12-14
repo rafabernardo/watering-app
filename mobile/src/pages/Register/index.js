a0d312e5e4a3a9bd757ac7dc5e2fb9fa4065f2da
velosobr <linoc.veloso@gmail.com>
2020-12-14 17:24:09 -0300

    save button functions

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -1,22 +1,78 @@
-import React from 'react';
-import { Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import React, { setState, useState, useEffect, useRef } from 'react';
+import { Text, View, TouchableOpacity, ToastAndroid } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
-import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
-
+import { postRegister } from '../../services/RegisterService'
+import styles from './styles';
 
 export default function Register() {
+   const [name, setName] = useState('')
+   const [species, setSpecies] = useState('')
+   const [locate, setLocate] = useState('')
+   const [humidity_level, setHumidity] = useState('')
+
+
    const navigation = useNavigation();
 
    function navigateBack() {
       navigation.goBack();
    }
-   async function handleAddRegister() {
-      const response = await api.post()
+   function navigateToHome() {
+      navigation.navigate('Pagina Inicial')
+   }
 
-      console.log("registro salvo com sucesso");
-      navigation.navigate('Pagina Inicial');
+
+   const nameFocus = useRef(null)
+
+   useEffect(() => {
+      nameFocus.current.focus();
+   }, [])
+
+   const onChangeName = (txtName) => {
+      setName(txtName)
+   }
+   const onChangeSpecie = (txtSpecie) => {
+      setSpecies(txtSpecie)
+   }
+   const onChangeHumidity = (txtHumidity) => {
+      setHumidity(txtHumidity)
+   }
+   const onChangeLocate = (txtLocate) => {
+      setLocate(txtLocate)
+   }
+   onSave = async () => {
+      let today = new Date();
+
+      let dd = String(today.getDate()).padStart(2, '0');
+      let mm = String(today.getMonth() + 1).padStart(2, '0');
+      let yyyy = today.getFullYear();
+
+      let hh = today.getHours();
+      let min = today.getMinutes();
+
+      min.length === 1 ? min = "0" + min : null;
+
+      let acquisition_date = `${dd}/${mm}/${yyyy} ${hh}:${min}`;
+
+      console.log("entrou no save");
+      postRegister({
+         name,
+         species,
+         locate,
+         humidity_level,
+         acquisition_date
+      }).then(() => {
+         console.log("entrou no then save");
+
+         setName('')
+         setSpecies('')
+         setLocate('')
+         setHumidity('')
+         navigation.navigate('Pagina Inicial')
+      }).catch(err => {
+         console.log('Erro: ', err);
+      })
    }
 
    return (
@@ -36,35 +92,37 @@ export default function Register() {
             <View style={styles.regItem}>
                <Text style={styles.regTitle}>Nome da planta</Text>
                <TextInput style={styles.regInput}
-                  onChangeText={() => { }}
                   placeholder="Digite o nome sua plantinha"
                   placeholderTextColor="#CCCCCC"
-
+                  ref={nameFocus}
+                  value={name}
+                  onChangeText={txtName => onChangeName(txtName)}
                />
             </View>
+
             <View style={styles.regItem}>
                <Text style={styles.regTitle}>Espécie</Text>
                <TextInput style={styles.regInput}
-                  onChangeText={() => { }}
                   placeholder="Digite o nome da espécie"
                   placeholderTextColor="#CCCCCC"
+                  value={species}
+                  onChangeText={txtSpecie => onChangeSpecie(txtSpecie)}
 
                />
             </View>
             <View style={styles.regItem}>
                <Text style={styles.regTitle}>Nivel de umidade</Text>
                <TextInput style={styles.regInput}
-                  onChangeText={() => { }}
                   placeholder="Digite o nivel de umidade necessaria"
                   placeholderTextColor="#CCCCCC"
-
+                  value={humidity_level}
+                  onChangeText={txtHumidity => onChangeHumidity(txtHumidity)}
                />
             </View>
             <View style={styles.regItem}>
                <Text style={styles.regTitle}>data de aquisição</Text>
                <Text style={styles.regInput}
-                  onChangeText={() => { }}
-                  placeholder="Digite o nome da espécie"
+                  placeholder="Digite a data de aquisição"
                   placeholderTextColor="#CCCCCC"
 
                />
@@ -73,20 +131,20 @@ export default function Register() {
             <View style={styles.regItem}>
                <Text style={styles.regTitle}>Localição</Text>
                <TextInput style={styles.regInput}
-                  onChangeText={() => { }}
                   placeholder="selecionar a localização"
                   placeholderTextColor="#CCCCCC"
+                  value={locate}
+                  onChangeText={txtLocate => onChangeLocate(txtLocate)}
 
                />
             </View>
-
 
          </View>
 
          <TouchableOpacity
             activeOpacity={0.6}
             style={styles.saveButton}
-            onPress={handleAddRegister}
+            onPress={onSave}
             color="#FF9933">
             <Text style={styles.textSaveButton}
             >
@@ -97,3 +155,4 @@ export default function Register() {
       </View>
    )
 }
+
